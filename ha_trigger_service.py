40b05777e3b5043c696ec761c1943c1a2015a542
Andy Fraley <andrew.fraley@gmail.com>
2021-11-23 19:00:20 -0500

    Create ha_trigger_service.py

---
+++ ha_trigger_service.py
@@ -0,0 +1,84 @@
+"""
+    Script to trigger HA services
+
+    By default this script looks for a config file in default location /etc/ha_config
+    ha_config file should look like:
+
+    url=http://homeassistant.local:8123/api/
+    token=12345678910abcdefghijklmnopqrstuvwxyz
+
+    Install Deps:
+    python3 -m pip install homeassistant_api
+    
+    Usage:
+    python3 ha_trigger_service.py --service scene.turn_on --entity scene.my_scene
+
+    Specify config file location:
+    python3 ha_trigger_service.py --config config_file_path --service media_player.turn_off --entity media_player.sonytv
+
+"""
+
+import sys
+import argparse
+import configparser
+from homeassistant_api import Client as HAClient
+
+# pylint: disable=line-too-long
+
+
+def main():
+    """ MAIN """
+    args = get_args()
+    config = get_config(args.config)
+
+    trigger_service(service=args.service, entity_id=args.entity, config=config)
+
+
+def trigger_service(service, entity_id, config):
+    """ Use the ha client lib to trigger the scene """
+    client = HAClient(
+        config['url'],
+        config['token']
+    )
+    services = client.get_domains()
+
+    service_name = service.split('.')[0]
+    service_action = service.split('.')[1]
+    service_domain = services[service_name]
+    getattr(service_domain, service_action).trigger(entity_id=entity_id)
+
+
+def get_config(path):
+    """ Get INI formatted config from path """
+    parser = configparser.ConfigParser()
+    with open(path, encoding="utf8") as stream:
+        parser.read_string("[default]\n" + stream.read())
+
+    config = dict(parser['default'])
+
+    if not config['url']:
+        sys.exit(f'No url found in config file {path}')
+
+    if not config['token']:
+        sys.exit(f'No token found in config file {path}')
+
+    return config
+
+
+def get_args():
+    """ Get argparser args """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--service', help='Service to call', required=True)
+    parser.add_argument('--entity', help='entity_id target', required=True)
+    parser.add_argument('--config', metavar='config_file_path', help='Path to config file', required=False, default='/etc/ha_config')
+    parser.add_argument('--debug', help='Enable debug logging', action='store_true', required=False, default=False)
+    args = parser.parse_args()
+
+    if len(args.service.split('.')) < 2:
+        sys.exit('--service should be in the format of service_name.service_action i.e. media_player.turn_off')
+
+    return args
+
+
+if __name__ == "__main__":
+    main()
